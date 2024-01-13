bf20aa0eb1d0f3607befa5f631d2b6eb44a7bd25
edpassos <epassos@blatstudio.com>
2024-01-13 13:36:35 +0000

    changed require order, to install plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
+require("core.plugins")
+require("core.plugin_config")
 require("core.colors")
 require("core.options")
 require("core.keymaps")
-require("core.plugins")
-require("core.plugin_config")
