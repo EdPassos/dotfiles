32f8eaba9dad89fefc39c3309c3792b4f2e49b5c
edpassos <epassos@blatstudio.com>
2024-01-13 13:29:01 +0000

    instal nvim packer

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 brew bundle
 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 mkdir ~/.config
 cp -rf nvim ~/.config/.
 cp -rf tmux ~/.config/.
